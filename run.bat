27d593b061662f50c4021e9595900b7963fde97e
sunjxf <s_345@qq.com>
2022-12-07 10:15:06 +0800

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal EnableDelayedExpansion
+ 
+
+del b.vcf
+ 
+set /a line=1
+for /f "tokens=1,2,3,4 delims=," %%a in (a.csv) do (
+        (echo BEGIN:VCARD)       >> b.vcf
+        (echo VERSION:3.0)       >> b.vcf
+        (echo FN:%%b)            >> b.vcf
+        (echo TEL;TYPE=WORK,CELL:%%a) >> b.vcf
+        (echo TITLE:%%c)           >> b.vcf
+        (echo NOTE:%%d)         >> b.vcf
+        (echo END:VCARD)         >> b.vcf
+    set /a line+=1
+)
+ 
